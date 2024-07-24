14f89fc761a1aa4aa5adea9e7ae66b5863c5fab9
Deon <nuvend@gmail.com>
2024-07-24 11:08:34 +0200

    CHANGES

--- Modules/Product/Resources/views/products/index.blade.php
+++ Modules/Product/Resources/views/products/index.blade.php
@@ -25,10 +25,10 @@
                                 <h1>SITE BREAKDOWN</h1>
                             </div>
                             <div class="row justify-content-center" style='text-align:center;'>
-                                <h1>TOAL COIN IN - <b>{{$coinIn}}</b></h1>
+                                <h1>TOAL IN: <b>{{$coinIn}}</b></h1>
                             </div>
                             <div class="row justify-content-center" style='text-align:center;'>
-                                <h1>TOAL COIN OUT - <b>{{$coinOut}}</b></h1>
+                                <h1>TOAL OUT: <b>{{$coinOut}}</b></h1>
                             </div>
                         <hr>
                         <!-- Button trigger modal -->
